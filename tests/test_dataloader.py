5d6870acdd6ea1553530b371f31d14785abbda8e
Nassim Oufattole <nassim@orcrist.partners.org>
2024-07-05 20:17:31 +0000

    added task indexing of time series data. i.e. Given labels and start and end dates defined in a dataframe for each patient, we can retrieve the subset of dynamic data available for that start to end span for the patient and associate it with the label.

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -3,8 +3,10 @@ import rootutils
 root = rootutils.setup_root(__file__, dotenv=True, pythonpath=True, cwd=True)
 
 import shutil
+from datetime import datetime
 from pathlib import Path
 
+import polars as pl
 import torch
 from hydra import compose, initialize
 
@@ -62,6 +64,16 @@ def test_triplet(tmp_path):
     # triplet collating
     pyd = PytorchDataset(cfg, split="train")
     batch = pyd.collate([pyd[i] for i in range(2)])
+    assert batch.keys() == {
+        "mask",
+        "static_mask",
+        "code",
+        "numerical_value",
+        "time_delta_days",
+        "numerical_value_mask",
+    }
+    for key in batch.keys():
+        assert batch[key].shape == torch.Size([2, 17])
 
     # check the continuous value embedder works
     data = torch.ones((2, 3), dtype=torch.float32)
@@ -75,3 +87,58 @@ def test_triplet(tmp_path):
     assert embedding.shape == torch.Size([2, 4, 17])
     # Check that the mask is shape batch size x sequence length
     assert mask.shape == torch.Size([2, 17])
+
+
+def test_task(tmp_path):
+    MEDS_cohort_dir = tmp_path / "processed" / "final_cohort"
+    shutil.copytree(Path("./tests/test_data"), MEDS_cohort_dir.parent)
+
+    task_name = "bababooey"
+    kwargs = {
+        "raw_MEDS_cohort_dir": str(MEDS_cohort_dir.parent.resolve()),
+        "MEDS_cohort_dir": str(MEDS_cohort_dir.resolve()),
+        "max_seq_len": 512,
+        "embedder.token_dim": 4,
+        "collate_type": "triplet",
+        "task_name": task_name,
+    }
+
+    task_df = pl.DataFrame(
+        {
+            "patient_id": [239684, 1195293, 68729, 814703],
+            "start_time": [
+                datetime(1980, 12, 28),
+                datetime(1978, 6, 20),
+                datetime(1978, 3, 9),
+                datetime(1976, 3, 28),
+            ],
+            "end_time": [
+                datetime(2010, 5, 11, 18, 25, 35),
+                datetime(2010, 6, 20, 20, 12, 31),
+                datetime(2010, 5, 26, 2, 30, 56),
+                datetime(2010, 2, 5, 5, 55, 39),
+            ],
+            "label": [0, 1, 0, 1],
+        }
+    )
+    tasks_dir = MEDS_cohort_dir / "tasks"
+    tasks_dir.mkdir(parents=True, exist_ok=True)
+    task_df.write_parquet(tasks_dir / f"{task_name}.parquet")
+
+    with initialize(version_base=None, config_path="../src/MEDS_torch/configs"):  # path to config.yaml
+        overrides = [f"{k}={v}" for k, v in kwargs.items()]
+        cfg = compose(config_name="pytorch_dataset", overrides=overrides)  # config.yaml
+
+    # triplet collating
+    pyd = PytorchDataset(cfg, split="train")
+    batch = pyd.collate([pyd[i] for i in range(2)])
+    assert batch.keys() == {
+        "mask",
+        "static_mask",
+        "code",
+        "numerical_value",
+        "time_delta_days",
+        "numerical_value_mask",
+    }
+    for key in batch.keys():
+        assert batch[key].shape == torch.Size([2, 10])
