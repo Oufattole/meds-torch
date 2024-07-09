4c8fbf1df37ee4c213c6550a4a1b53a31612b4ea
Nassim <noufattole@gmail.com>
2024-07-09 17:14:17 -0400

    added a pytest for clmbr

--- tests/test_clmbr.py
+++ tests/test_clmbr.py
@@ -3,349 +3,115 @@ import os
 from io import StringIO
 from pathlib import Path
 
+import pyarrow
+import pyarrow.parquet
+import meds
+import tempfile
+import datasets
+import polars as pl
+import datetime
 import polars as pl
 from hydra import compose, initialize
+import femr.index
+import femr.splits
+import os
+import femr.models.tokenizer
+import femr.models.processor
+import femr.models.tasks
 
 from MEDS_torch.clmbr.clmbr_model import CLMBR
 from MEDS_torch.utils import list_subdir_files
 
-SPLITS_JSON = """{"train/0": [239684, 1195293], "train/1": [68729, 814703], "tuning/0": [754281], "held_out/0": [1500733]}"""  # noqa: E501
-
-MEDS_TRAIN_0 = """
-patient_id,code,timestamp,numerical_value
-239684,HEIGHT,,175.271115221764
-239684,EYE_COLOR//BROWN,,
-239684,DOB,1980-12-28T00:00:00.000000,
-239684,TEMP,2010-05-11T17:41:51.000000,96.0
-239684,ADMISSION//CARDIAC,2010-05-11T17:41:51.000000,
-239684,HR,2010-05-11T17:41:51.000000,102.6
-239684,TEMP,2010-05-11T17:48:48.000000,96.2
-239684,HR,2010-05-11T17:48:48.000000,105.1
-239684,TEMP,2010-05-11T18:25:35.000000,95.8
-239684,HR,2010-05-11T18:25:35.000000,113.4
-239684,HR,2010-05-11T18:57:18.000000,112.6
-239684,TEMP,2010-05-11T18:57:18.000000,95.5
-239684,DISCHARGE,2010-05-11T19:27:19.000000,
-1195293,HEIGHT,,164.6868838269085
-1195293,EYE_COLOR//BLUE,,
-1195293,DOB,1978-06-20T00:00:00.000000,
-1195293,TEMP,2010-06-20T19:23:52.000000,100.0
-1195293,ADMISSION//CARDIAC,2010-06-20T19:23:52.000000,
-1195293,HR,2010-06-20T19:23:52.000000,109.0
-1195293,TEMP,2010-06-20T19:25:32.000000,100.0
-1195293,HR,2010-06-20T19:25:32.000000,114.1
-1195293,HR,2010-06-20T19:45:19.000000,119.8
-1195293,TEMP,2010-06-20T19:45:19.000000,99.9
-1195293,HR,2010-06-20T20:12:31.000000,112.5
-1195293,TEMP,2010-06-20T20:12:31.000000,99.8
-1195293,HR,2010-06-20T20:24:44.000000,107.7
-1195293,TEMP,2010-06-20T20:24:44.000000,100.0
-1195293,TEMP,2010-06-20T20:41:33.000000,100.4
-1195293,HR,2010-06-20T20:41:33.000000,107.5
-1195293,DISCHARGE,2010-06-20T20:50:04.000000,
-"""
-MEDS_TRAIN_1 = """
-patient_id,code,timestamp,numerical_value
-68729,EYE_COLOR//HAZEL,,
-68729,HEIGHT,,160.3953106166676
-68729,DOB,1978-03-09T00:00:00.000000,
-68729,HR,2010-05-26T02:30:56.000000,86.0
-68729,ADMISSION//PULMONARY,2010-05-26T02:30:56.000000,
-68729,TEMP,2010-05-26T02:30:56.000000,97.8
-68729,DISCHARGE,2010-05-26T04:51:52.000000,
-814703,EYE_COLOR//HAZEL,,
-814703,HEIGHT,,156.48559093209357
-814703,DOB,1976-03-28T00:00:00.000000,
-814703,TEMP,2010-02-05T05:55:39.000000,100.1
-814703,HR,2010-02-05T05:55:39.000000,170.2
-814703,ADMISSION//ORTHOPEDIC,2010-02-05T05:55:39.000000,
-814703,DISCHARGE,2010-02-05T07:02:30.000000,
-"""
-MEDS_HELD_OUT_0 = """
-patient_id,code,timestamp,numerical_value
-1500733,HEIGHT,,158.60131573580904
-1500733,EYE_COLOR//BROWN,,
-1500733,DOB,1986-07-20T00:00:00.000000,
-1500733,TEMP,2010-06-03T14:54:38.000000,100.0
-1500733,HR,2010-06-03T14:54:38.000000,91.4
-1500733,ADMISSION//ORTHOPEDIC,2010-06-03T14:54:38.000000,
-1500733,HR,2010-06-03T15:39:49.000000,84.4
-1500733,TEMP,2010-06-03T15:39:49.000000,100.3
-1500733,HR,2010-06-03T16:20:49.000000,90.1
-1500733,TEMP,2010-06-03T16:20:49.000000,100.1
-1500733,DISCHARGE,2010-06-03T16:44:26.000000,
-"""
-MEDS_TUNING_0 = """
-patient_id,code,timestamp,numerical_value
-754281,EYE_COLOR//BROWN,,
-754281,HEIGHT,,166.22261567137025
-754281,DOB,1988-12-19T00:00:00.000000,
-754281,ADMISSION//PULMONARY,2010-01-03T06:27:59.000000,
-754281,TEMP,2010-01-03T06:27:59.000000,99.8
-754281,HR,2010-01-03T06:27:59.000000,142.0
-754281,DISCHARGE,2010-01-03T08:22:13.000000,
-"""
-
-MEDS_OUTPUTS = {
-    "train/0": MEDS_TRAIN_0,
-    "train/1": MEDS_TRAIN_1,
-    "held_out/0": MEDS_HELD_OUT_0,
-    "tuning/0": MEDS_TUNING_0,
-}
-
-TRAIN_0_SCHEMA = {
-    "patient_id": [239684, 1195293],
-    "code": [[10, 8], [10, 7]],
-    "numerical_value": [[1.577007, None], [0.068028, None]],
-    "start_time": [
-        "1980-12-28 00:00:00",
-        "1978-06-20 00:00:00",
-    ],
-    "timestamp": [
-        [
-            "1980-12-28 00:00:00",
-            "2010-05-11 17:41:51",
-            "2010-05-11 17:48:48",
-            "2010-05-11 18:25:35",
-            "2010-05-11 18:57:18",
-            "2010-05-11 19:27:19",
-        ],
-        [
-            "1978-06-20 00:00:00",
-            "2010-06-20 19:23:52",
-            "2010-06-20 19:25:32",
-            "2010-06-20 19:45:19",
-            "2010-06-20 20:12:31",
-            "2010-06-20 20:24:44",
-            "2010-06-20 20:41:33",
-            "2010-06-20 20:50:04",
-        ],
-    ],
-}
-
-TRAIN_1_SCHEMA = {
-    "patient_id": [68729, 814703],
-    "code": [[9, 10], [9, 10]],
-    "numerical_value": [[None, -0.543816], [None, -1.101219]],
-    "start_time": ["1978-03-09 00:00:00", "1976-03-28 00:00:00"],
-    "timestamp": [
-        ["1978-03-09 00:00:00", "2010-05-26 02:30:56", "2010-05-26 04:51:52"],
-        ["1976-03-28 00:00:00", "2010-02-05 05:55:39", "2010-02-05 07:02:30"],
-    ],
-}
-
-HELDOUT_0_SCHEMA = {
-    "patient_id": [1500733],
-    "code": [[10, 8]],
-    "numerical_value": [[-0.799583, None]],
-    "start_time": ["1986-07-20 00:00:00"],
-    "timestamp": [
-        [
-            "1986-07-20 00:00:00",
-            "2010-06-03 14:54:38",
-            "2010-06-03 15:39:49",
-            "2010-06-03 16:20:49",
-            "2010-06-03 16:44:26",
-        ]
-    ],
-}
-
-TUNING_0_SCHEMA = {
-    "patient_id": [754281],
-    "code": [[8, 10]],
-    "numerical_value": [[None, 0.286975]],
-    "start_time": ["1988-12-19 00:00:00"],
-    "timestamp": [["2010-01-03 06:27:59", "2010-01-03 08:22:13", "1988-12-19 00:00:00"]],
-}
-
-
-def process_schema(schema):
-    schema = pl.DataFrame(schema)
-    schema = schema.with_columns(
-        [
-            pl.col("start_time").str.strptime(pl.Datetime, format="%Y-%m-%d %H:%M:%S"),
-            pl.col("timestamp").list.eval(
-                pl.element().str.strptime(pl.Datetime, format="%Y-%m-%d %H:%M:%S"), parallel=True
-            ),
-        ]
-    )
-    return schema
-
-
-def write_schema_df(schema_df, MEDS_cohort_dir, split, shard):
-    output_dir = MEDS_cohort_dir / "tokenization" / "schemas" / split
-    os.makedirs(output_dir, exist_ok=True)
-    schema_df.write_parquet(output_dir / f"{shard}.parquet")
-
-
-import numpy as np
-from nested_ragged_tensors.ragged_numpy import JointNestedRaggedTensorDict
-
-# Define the data using numpy arrays for better handling of numerical data and NaN values
-TRAIN_0_NRT_DATA = {
-    "dim1/bounds": np.array([6, 14]),
-    "dim1/lengths": np.array([6, 8]),
-    "dim1/time_delta_days": [
-        np.array(
-            [np.nan, 1.0726737e04, 4.8263888e-03, 2.5543982e-02, 2.2025462e-02, 2.0844907e-02],
-            dtype=np.float32,
-        ),
-        np.array(
-            [
-                np.nan,
-                1.1688809e04,
-                1.1574074e-03,
-                1.3738426e-02,
-                1.8888889e-02,
-                8.4837964e-03,
-                1.1678241e-02,
-                5.9143519e-03,
-            ],
-            dtype=np.float32,
-        ),
-    ],
-    "dim2/bounds": np.array([1, 4, 6, 8, 10, 11, 12, 15, 17, 19, 21, 23, 25, 26]),
-    "dim2/code": [
-        np.array([6.0], dtype=np.float32),
-        np.array([12.0, 2.0, 11.0], dtype=np.float32),
-        np.array([12.0, 11.0], dtype=np.float32),
-        np.array([12.0, 11.0], dtype=np.float32),
-        np.array([11.0, 12.0], dtype=np.float32),
-        np.array([5.0], dtype=np.float32),
-        np.array([6.0], dtype=np.float32),
-        np.array([12.0, 2.0, 11.0], dtype=np.float32),
-        np.array([12.0, 11.0], dtype=np.float32),
-        np.array([11.0, 12.0], dtype=np.float32),
-        np.array([11.0, 12.0], dtype=np.float32),
-        np.array([11.0, 12.0], dtype=np.float32),
-        np.array([12.0, 11.0], dtype=np.float32),
-        np.array([5.0], dtype=np.float32),
-    ],
-    "dim2/lengths": np.array([1, 3, 2, 2, 2, 1, 1, 3, 2, 2, 2, 2, 2, 1]),
-    "dim2/numerical_value": [
-        np.array([np.nan], dtype=np.float32),
-        np.array([-1.2713274, np.nan, -0.5697363], dtype=np.float32),
-        np.array([-1.1678973, -0.43754688], dtype=np.float32),
-        np.array([-1.3747574, 1.3218939e-03], dtype=np.float32),
-        np.array([-0.04097871, -1.5299025], dtype=np.float32),
-        np.array([np.nan], dtype=np.float32),
-        np.array([np.nan], dtype=np.float32),
-        np.array([0.7972731, np.nan, -0.23133144], dtype=np.float32),
-        np.array([0.7972731, 0.03833492], dtype=np.float32),
-        np.array([0.33972675, 0.7455581], dtype=np.float32),
-        np.array([-0.04626629, 0.69384307], dtype=np.float32),
-        np.array([-0.30006993, 0.7972731], dtype=np.float32),
-        np.array([1.0041331, -0.31064507], dtype=np.float32),
-        np.array([np.nan], dtype=np.float32),
-    ],
-}
-
-TRAIN_0_NRT_SCHEMA = {
-    "dim1/time_delta_days": np.dtype("float32"),
-    "dim2/code": np.dtype("float32"),
-    "dim2/numerical_value": np.dtype("float32"),
-}
-JNRT_TRAIN_0 = JointNestedRaggedTensorDict(TRAIN_0_NRT_DATA, TRAIN_0_NRT_SCHEMA, pre_raggedified=True)
-
-import numpy as np
-from nested_ragged_tensors.ragged_numpy import JointNestedRaggedTensorDict
-
-# Define the data using numpy arrays to handle numerical data and NaN values effectively
-TRAIN_1_NRT_DATA = {
-    "dim1/bounds": np.array([3, 6]),
-    "dim1/lengths": np.array([3, 3]),
-    "dim1/time_delta_days": [
-        np.array([np.nan, 1.17661045e04, 9.78703722e-02], dtype=np.float32),
-        np.array([np.nan, 1.2367247e04, 4.6423610e-02], dtype=np.float32),
-    ],
-    "dim2/bounds": np.array([1, 4, 5, 6, 9, 10]),
-    "dim2/code": [
-        np.array([6.0], dtype=np.float32),
-        np.array([11.0, 4.0, 12.0], dtype=np.float32),
-        np.array([5.0], dtype=np.float32),
-        np.array([6.0], dtype=np.float32),
-        np.array([12.0, 11.0, 3.0], dtype=np.float32),
-        np.array([5.0], dtype=np.float32),
-    ],
-    "dim2/lengths": np.array([1, 3, 1, 1, 3, 1]),
-    "dim2/numerical_value": [
-        np.array([np.nan], dtype=np.float32),
-        np.array([-1.4474739, np.nan, -0.34045714], dtype=np.float32),
-        np.array([np.nan], dtype=np.float32),
-        np.array([np.nan], dtype=np.float32),
-        np.array([0.8489881, 3.004665, np.nan], dtype=np.float32),
-        np.array([np.nan], dtype=np.float32),
-    ],
-}
-
-TRAIN_1_NRT_SCHEMA = {
-    "dim1/time_delta_days": np.dtype("float32"),
-    "dim2/code": np.dtype("float32"),
-    "dim2/numerical_value": np.dtype("float32"),
-}
-
-# Create the JointNestedRaggedTensorDict object
-JNRT_TRAIN_1 = JointNestedRaggedTensorDict(TRAIN_1_NRT_DATA, TRAIN_1_NRT_SCHEMA, pre_raggedified=True)
 
 
 def test_clmbr(tmp_path):
-    MEDS_cohort_dir = tmp_path / "processed" / "final_cohort"
-    describe_codes_config = {
-        "raw_MEDS_cohort_dir": str(MEDS_cohort_dir.parent.resolve()),
-        "MEDS_cohort_dir": str(MEDS_cohort_dir.resolve()),
-        "max_seq_len": 512,
-    }
-
-    with initialize(version_base=None, config_path="../src/MEDS_torch/configs"):  # path to config.yaml
-        overrides = [f"{k}={v}" for k, v in describe_codes_config.items()]
-        cfg = compose(config_name="pytorch_dataset", overrides=overrides)  # config.yaml
-
-    # Create the directories
-    MEDS_cohort_dir.mkdir(parents=True, exist_ok=True)
-
-    # Store MEDS outputs
-    for split, data in MEDS_OUTPUTS.items():
-        file_path = MEDS_cohort_dir / f"{split}.parquet"
-        file_path.parent.mkdir(exist_ok=True)
-        df = pl.read_csv(StringIO(data))
-        df.with_columns(pl.col("timestamp").str.to_datetime("%Y-%m-%dT%H:%M:%S%.f")).write_parquet(file_path)
-
-    # Check the files are not empty
-    meds_files = list_subdir_files(Path(cfg.MEDS_cohort_dir), "parquet")
-    assert (
-        len(list_subdir_files(Path(cfg.MEDS_cohort_dir).parent, "parquet")) == 4
-    ), "MEDS train split Data Files Should be 4!"
-    for f in meds_files:
-        assert pl.read_parquet(f).shape[0] > 0, "MEDS Data Tabular Dataframe Should not be Empty!"
-    split_json = json.load(StringIO(SPLITS_JSON))
-    splits_fp = MEDS_cohort_dir.parent / "splits.json"
-    json.dump(split_json, splits_fp.open("w"))
-
-    # train_0_schema_df = process_schema(TRAIN_0_SCHEMA)
-    # train_1_schema_df = process_schema(TRAIN_1_SCHEMA)
-    # # heldout_0_schema_df = process_schema(HELDOUT_0_SCHEMA)
-    # # tuning_0_schema_df = process_schema(TUNING_0_SCHEMA)
-    # write_schema_df(train_0_schema_df, MEDS_cohort_dir, "train", "0")
-    # write_schema_df(train_1_schema_df, MEDS_cohort_dir, "train", "1")
-    # tensorize_dir = MEDS_cohort_dir / "tensorize"
-    # os.makedirs(tensorize_dir / "train", exist_ok=True)
-    # JNRT_TRAIN_0.save(tensorize_dir / "train/0.nrt")
-    # JNRT_TRAIN_1.save(tensorize_dir / "train/1.nrt")
-
-    # pyd = PytorchDataset(cfg, split="train")
-
-    # Get an item:
-    # item = pyd[0]
-
-    # Get a batch:
-    # batch = pyd.collate([pyd[i] for i in range(2)])
+    input_meds = pl.read_parquet("tests/processed/final_cohort/**/*").rename(dict(timestamp="time", numerical_value="numeric_value"))
+    # impute time for static data with oldest time -- femr doesn't seem to support static data without this
+    input_meds = input_meds.group_by("patient_id").agg([pl.col("time").fill_null(pl.col("time").min()), "code", "numeric_value"]).explode("time", "code", "numeric_value")
+    # add a birth date
+    fake_bdays = input_meds.group_by("patient_id").agg(pl.col("time").min())
+    fake_bdays = fake_bdays.with_columns(pl.lit(meds.birth_code).alias("code"), pl.lit(None).alias("numeric_value"))
+    input_meds = pl.concat([input_meds, fake_bdays])
 
 
-MEDS_cohort_dir = Path("tests") / "processed" / "final_cohort"
-clmbr = CLMBR(target_dir="tests/clmbr")
-datasets = clmbr.load_datasets(MEDS_cohort_dir)
-transformer_cfg, tokenizer = clmbr.train_tokenizer(datasets)
-train_batches, val_batches, processor, clmbr_task = clmbr.create_batches(tokenizer, datasets)
-model = clmbr.train_model(transformer_cfg, clmbr_task, processor, train_batches, val_batches)
-# evaluation_results = clmbr.evaluate_model(model, processor, datasets)
-# print("Evaluation Results:", evaluation_results)
+    def unnest_df(df):
+        return df.explode("events").unnest("events").explode("measurements").unnest("measurements")
+
+    def nest(df: pl.DataFrame)-> pl.DataFrame:
+        """An older version of meds used this nested structure
+        
+        This function reconstructs the nested structure by grouping by patient ids and subnesting by event times
+
+        Args:
+            df (pl.DataFrame): 
+
+        Returns:
+            pl.DataFrame: double nested meds dataframe
+        """
+        # Step 1: Group by patient_id and time, then aggregate the relevant fields into structs
+        nested_measurements = df.group_by(["patient_id", "time"]).agg([
+            pl.struct(pl.exclude(["patient_id", "time"])).alias("measurements")
+        ])
+
+        # Step 2: Group by patient_id and aggregate the structs into lists
+        nested_events = nested_measurements.group_by("patient_id").agg([
+            pl.struct(["time", "measurements"]).alias("events")
+        ])
+        return nested_events
+
+    def convert_meds_to_femr_meds(df):
+        # confirm collumns are allows
+        allowed_columns = {'patient_id', 'time', 'code', 'text_value', 'numeric_value', 'datetime_value', 'metadata'}
+        assert all([each in allowed_columns for each in df.columns])
+        return nest(df)
+    df = convert_meds_to_femr_meds(input_meds)
+
+    patient_schema = meds.patient_schema()
+    patient_table = pyarrow.Table.from_pylist(df.to_dicts(), patient_schema)
+    parquet_path = f"{tmp_path}/test.parquet"
+    pyarrow.parquet.write_table(patient_table, parquet_path)
+    dataset = datasets.Dataset.from_parquet(f"{tmp_path}/*")
+    
+    
+    index = femr.index.PatientIndex(dataset, num_proc=4)
+    main_split = femr.splits.generate_hash_split(index.get_patient_ids(), 97, frac_test=0.15)
+
+
+    os.mkdir(os.path.join(tmp_path, 'clmbr_model'))
+    # Note that we want to save this to the target directory since this is important information
+
+    main_split.save_to_csv(os.path.join(tmp_path, "clmbr_model", "main_split.csv"))
+
+    train_split = femr.splits.generate_hash_split(main_split.train_patient_ids, 87, frac_test=0.15)
+
+    print(train_split.train_patient_ids)
+    print(train_split.test_patient_ids)
+
+    main_dataset = main_split.split_dataset(dataset, index)
+    train_dataset = train_split.split_dataset(main_dataset['train'], femr.index.PatientIndex(main_dataset['train'], num_proc=4))
+
+    print(train_dataset)
+    # First, we need to train a tokenizer
+    # NOTE: A vocab size of 128 is probably too low for a real model. 128 was chosen to make this tutorial quick to run
+    tokenizer = femr.models.tokenizer.train_tokenizer(
+        main_dataset['train'], vocab_size=128, num_proc=4)
+
+    # Save the tokenizer to the same directory as the model
+    tokenizer.save_pretrained(os.path.join(tmp_path, "clmbr_model"))
+
+
+
+    # Second, we need to create batches. We define the CLMBR task at this time
+
+    clmbr_task = femr.models.tasks.CLMBRTask(clmbr_vocab_size=64)
+
+    processor = femr.models.processor.FEMRBatchProcessor(tokenizer, clmbr_task)
+
+    # We can do this one patient at a time
+    example_batch = processor.collate([processor.convert_patient(train_dataset['train'][0], tensor_type='pt')])
+
+    # # But generally we want to convert entire datasets
+    # train_batches = processor.convert_dataset(train_dataset, tokens_per_batch=2, num_proc=1)
+
+    # # Convert our batches to pytorch tensors
+    # train_batches.set_format("pt")
