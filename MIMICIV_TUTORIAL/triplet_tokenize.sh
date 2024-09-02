60dfeef17c114bee9eb715a15c9a0ff343fe383d
Nassim Oufattole <noufattole@gmail.com>
2024-09-01 21:44:07 -0400

    removed commented out script code that is needed for the triplet tokenization

--- MIMICIV_TUTORIAL/triplet_tokenize.sh
+++ MIMICIV_TUTORIAL/triplet_tokenize.sh
@@ -39,94 +39,94 @@ N_PARALLEL_WORKERS="$3"
 CONFIG_DIR="$4"
 CONFIG_NAME="$5"
 
-# echo "Converting to code metadata..."
-# MEDS_transform-aggregate_code_metadata --multirun \
-#     --config-path="$CONFIG_DIR" \
-#     --config-name="$CONFIG_NAME" \
-#     worker="range(0,${N_PARALLEL_WORKERS})" \
-#     input_dir="$MEDS_DIR" \
-#     cohort_dir="$MODEL_DIR" \
-#     stage="aggregate_code_metadata" \
-#     hydra.searchpath="[pkg://MEDS_transforms.configs]" \
-#     polling_time=5
+echo "Converting to code metadata..."
+MEDS_transform-aggregate_code_metadata --multirun \
+    --config-path="$CONFIG_DIR" \
+    --config-name="$CONFIG_NAME" \
+    worker="range(0,${N_PARALLEL_WORKERS})" \
+    input_dir="$MEDS_DIR" \
+    cohort_dir="$MODEL_DIR" \
+    stage="aggregate_code_metadata" \
+    hydra.searchpath="[pkg://MEDS_transforms.configs]" \
+    polling_time=5
 
-# echo "Filtering subjects..."
-# MEDS_transform-filter_subjects --multirun \
-#     --config-path="$CONFIG_DIR" \
-#     --config-name="$CONFIG_NAME" \
-#     worker="range(0,${N_PARALLEL_WORKERS})" \
-#     input_dir="$MEDS_DIR" \
-#     cohort_dir="$MODEL_DIR" \
-#     stage="filter_subjects" \
-#     hydra.searchpath="[pkg://MEDS_transforms.configs]"
+echo "Filtering subjects..."
+MEDS_transform-filter_subjects --multirun \
+    --config-path="$CONFIG_DIR" \
+    --config-name="$CONFIG_NAME" \
+    worker="range(0,${N_PARALLEL_WORKERS})" \
+    input_dir="$MEDS_DIR" \
+    cohort_dir="$MODEL_DIR" \
+    stage="filter_subjects" \
+    hydra.searchpath="[pkg://MEDS_transforms.configs]"
 
-# echo "Generating time derived measurements..."
-# MEDS_transform-add_time_derived_measurements --multirun \
-#     --config-path="$CONFIG_DIR" \
-#     --config-name="$CONFIG_NAME" \
-#     worker="range(0,${N_PARALLEL_WORKERS})" \
-#     input_dir="$MEDS_DIR" \
-#     cohort_dir="$MODEL_DIR" \
-#     stage="add_time_derived_measurements" \
-#     hydra.searchpath="[pkg://MEDS_transforms.configs]"
+echo "Generating time derived measurements..."
+MEDS_transform-add_time_derived_measurements --multirun \
+    --config-path="$CONFIG_DIR" \
+    --config-name="$CONFIG_NAME" \
+    worker="range(0,${N_PARALLEL_WORKERS})" \
+    input_dir="$MEDS_DIR" \
+    cohort_dir="$MODEL_DIR" \
+    stage="add_time_derived_measurements" \
+    hydra.searchpath="[pkg://MEDS_transforms.configs]"
 
-# echo "Filtering measurements..."
-# MEDS_transform-filter_measurements --multirun \
-#     --config-path="$CONFIG_DIR" \
-#     --config-name="$CONFIG_NAME" \
-#     worker="range(0,${N_PARALLEL_WORKERS})" \
-#     input_dir="$MEDS_DIR" \
-#     cohort_dir="$MODEL_DIR" \
-#     stage="filter_measurements" \
-#     hydra.searchpath="[pkg://MEDS_transforms.configs]"
+echo "Filtering measurements..."
+MEDS_transform-filter_measurements --multirun \
+    --config-path="$CONFIG_DIR" \
+    --config-name="$CONFIG_NAME" \
+    worker="range(0,${N_PARALLEL_WORKERS})" \
+    input_dir="$MEDS_DIR" \
+    cohort_dir="$MODEL_DIR" \
+    stage="filter_measurements" \
+    hydra.searchpath="[pkg://MEDS_transforms.configs]"
 
-# echo "Occluding outliers..."
-# MEDS_transform-occlude_outliers --multirun \
-#     --config-path="$CONFIG_DIR" \
-#     --config-name="$CONFIG_NAME" \
-#     worker="range(0,${N_PARALLEL_WORKERS})" \
-#     input_dir="$MEDS_DIR" \
-#     cohort_dir="$MODEL_DIR" \
-#     stage="occlude_outliers" \
-#     hydra.searchpath="[pkg://MEDS_transforms.configs]"
+echo "Occluding outliers..."
+MEDS_transform-occlude_outliers --multirun \
+    --config-path="$CONFIG_DIR" \
+    --config-name="$CONFIG_NAME" \
+    worker="range(0,${N_PARALLEL_WORKERS})" \
+    input_dir="$MEDS_DIR" \
+    cohort_dir="$MODEL_DIR" \
+    stage="occlude_outliers" \
+    hydra.searchpath="[pkg://MEDS_transforms.configs]"
 
-# # We rerun aggregate_code_metadata to remove the filtered out codes, it seems the filter_measurements stage does not do that in meds v0.0.6
-# rm -rf "$MODEL_DIR"/aggregate_code_metadata
-# rm -f $MODEL_DIR/metadata/codes.parquet
-# echo "Converting to code metadata..."
-# MEDS_transform-aggregate_code_metadata \
-#     --config-path="$CONFIG_DIR" \
-#     --config-name="$CONFIG_NAME" \
-#     input_dir="$MODEL_DIR" \
-#     cohort_dir="$MODEL_DIR" \
-#     stage="aggregate_code_metadata" \
-#     hydra.searchpath="[pkg://MEDS_transforms.configs]" \
-#     ++stage_configs.aggregate_code_metadata.data_input_dir="${MODEL_DIR}/occlude_outliers/" \
-#     ++stage_configs.aggregate_code_metadata.metadata_input_dir="${MODEL_DIR}/metadata/" \
-#     polling_time=5
+# We rerun aggregate_code_metadata to remove the filtered out codes, it seems the filter_measurements stage does not do that in meds v0.0.6
+rm -rf "${MODEL_DIR}/aggregate_code_metadata"
+rm -f "${MODEL_DIR}/metadata/codes.parquet"
+echo "Converting to code metadata..."
+MEDS_transform-aggregate_code_metadata \
+    --config-path="$CONFIG_DIR" \
+    --config-name="$CONFIG_NAME" \
+    input_dir="$MODEL_DIR" \
+    cohort_dir="$MODEL_DIR" \
+    stage="aggregate_code_metadata" \
+    hydra.searchpath="[pkg://MEDS_transforms.configs]" \
+    ++stage_configs.aggregate_code_metadata.data_input_dir="${MODEL_DIR}/occlude_outliers/" \
+    ++stage_configs.aggregate_code_metadata.metadata_input_dir="${MODEL_DIR}/metadata/" \
+    polling_time=5
 
-# mkdir -p $MODEL_DIR/metadata/
-# cp $MODEL_DIR/aggregate_code_metadata/codes.parquet $MODEL_DIR/metadata/codes.parquet
+mkdir -p "${MODEL_DIR}/metadata/"
+cp "${MODEL_DIR}/aggregate_code_metadata/codes.parquet" "${MODEL_DIR}/metadata/codes.parquet"
 
-# echo "Fitting vocabulary indices..."
-# MEDS_transform-fit_vocabulary_indices --multirun \
-#     --config-path="$CONFIG_DIR" \
-#     --config-name="$CONFIG_NAME" \
-#     worker="range(0,${N_PARALLEL_WORKERS})" \
-#     input_dir="$MEDS_DIR" \
-#     cohort_dir="$MODEL_DIR" \
-#     stage="fit_vocabulary_indices" \
-#     hydra.searchpath="[pkg://MEDS_transforms.configs]"
+echo "Fitting vocabulary indices..."
+MEDS_transform-fit_vocabulary_indices --multirun \
+    --config-path="$CONFIG_DIR" \
+    --config-name="$CONFIG_NAME" \
+    worker="range(0,${N_PARALLEL_WORKERS})" \
+    input_dir="$MEDS_DIR" \
+    cohort_dir="$MODEL_DIR" \
+    stage="fit_vocabulary_indices" \
+    hydra.searchpath="[pkg://MEDS_transforms.configs]"
 
-# echo "Normalizing data (converting codes to use integer encodings)..."
-# MEDS_transform-normalization --multirun \
-#     --config-path="$CONFIG_DIR" \
-#     --config-name="$CONFIG_NAME" \
-#     worker="range(0,${N_PARALLEL_WORKERS})" \
-#     input_dir="$MEDS_DIR" \
-#     cohort_dir="$MODEL_DIR" \
-#     stage="normalization" \
-#     hydra.searchpath="[pkg://MEDS_transforms.configs]"
+echo "Normalizing data (converting codes to use integer encodings)..."
+MEDS_transform-normalization --multirun \
+    --config-path="$CONFIG_DIR" \
+    --config-name="$CONFIG_NAME" \
+    worker="range(0,${N_PARALLEL_WORKERS})" \
+    input_dir="$MEDS_DIR" \
+    cohort_dir="$MODEL_DIR" \
+    stage="normalization" \
+    hydra.searchpath="[pkg://MEDS_transforms.configs]"
 
 
 echo "Converting to tokenization..."
